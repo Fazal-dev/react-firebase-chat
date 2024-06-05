8f98e67321cf6a1a9e7ccc217827fd6be6dac7e9
Shain922 <fmhd48763@gmail.com>
2024-06-05 12:00:08 +0530

    add: detail component intiality

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,7 +1,52 @@
 import React from "react";
 import "./detail.css";
 const Detail = () => {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="/avatar.png" />
+        <h2>fazal details</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat settings</span>
+            <img src="./arrowUp.png" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" />
+                <span>photo 2024 2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
