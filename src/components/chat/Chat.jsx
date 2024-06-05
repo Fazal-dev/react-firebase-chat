43f9c1334e0afc7a3dcf4ce982048393ca9a971b
Shain922 <fmhd48763@gmail.com>
2024-06-05 08:08:45 +0530

    add: complete design for chat component

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -24,7 +24,40 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus
+              voluptas nemo unde deleniti fugit nobis, a rerum magni modi
+              repellendus.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus
+              voluptas nemo unde deleniti fugit nobis, a rerum magni modi
+              repellendus.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus
+              voluptas nemo unde deleniti fugit nobis, a rerum magni modi
+              repellendus.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
       <div className="bottom">
         <div className="icons">
           <img src="./img.png" alt="" />
